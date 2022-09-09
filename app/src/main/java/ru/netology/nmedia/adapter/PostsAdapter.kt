c629543ef9f572c28fca0f9a45111c860de3107f
jkuzmina <ju.a.kuzmina@yandex.ru>
2022-09-09 10:42:13 +0400

    images

--- app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
+++ app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
@@ -3,6 +3,7 @@ package ru.netology.nmedia.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.PopupMenu
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -20,6 +21,7 @@ interface OnInteractionListener {
     fun onEdit(post: Post) {}
     fun onRemove(post: Post) {}
     fun onShare(post: Post) {}
+    fun onPhotoClick(post: Post) {}
 }
 
 class PostsAdapter(
@@ -58,6 +60,7 @@ class PostViewHolder(
                 .circleCrop()
                 .into(binding.avatar)
             if(post.attachment?.url != null) {
+                imageAttachment.isVisible = true
                 Glide.with(imageAttachment)
                     .load("${BuildConfig.BASE_URL}/media/${post.attachment?.url}")
                     .placeholder(R.drawable.ic_loading_100dp)
@@ -65,7 +68,11 @@ class PostViewHolder(
                     .timeout(10_000)
                     .fitCenter()
                     .into(binding.imageAttachment)
+            } else{
+                imageAttachment.isVisible = false
+                Glide.with(imageAttachment).clear(binding.imageAttachment)
             }
+
 
             menu.setOnClickListener {
                 PopupMenu(it.context, it).apply {
@@ -93,6 +100,10 @@ class PostViewHolder(
 
             share.setOnClickListener {
                 onInteractionListener.onShare(post)
+            }
+
+            imageAttachment.setOnClickListener {
+                onInteractionListener.onPhotoClick(post)
             }
         }
     }
